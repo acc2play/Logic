80b1cc0cfd6ded498ca3983342b6c288be1187d0
acc2play <acc4explay@gmail.com>
2025-09-18 13:05:54 +0700

    Добавьте файлы проекта.

---
+++ Logic/Logic/logic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantMenu.Core.Entities
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Grams { get; set; }
+        public DishCategory Category { get; set; }
+        public  Country country { get; set; }
+
+    }
+
+    public enum DishCategory
+    {
+        Breakfast,
+        Salad,
+        MainCourse,
+        Dessert,
+        Drink
+    }
+    public enum Country
+    {
+        Russian,
+        Italian,
+        Eastern
+    }
+
+}
